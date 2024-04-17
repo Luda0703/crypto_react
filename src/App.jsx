0960a0bdacbb953924f8a145c62ab7b7ea056f9e
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-04-17 09:50:19 +0300

    CryptoContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Layout } from 'antd';
 import AppHeader from './components/layout/AppHeader';
 import AppSider from './components/layout/AppSider';
 import AppContent from './components/layout/AppContent.jsx';
+import { CryptoContextProvider } from './context/crypto-context';
 
 function App() {
   return (
-    <Layout>
+    <CryptoContextProvider>
+      <Layout>
       <AppHeader/>
       <Layout>
         <AppSider/>
         <AppContent/>
       </Layout>
     </Layout>
+    </CryptoContextProvider>
   )
 }
 
